e1d0f429119d31f7f946201be58299b52223bd79
Mohammed Amine OUASMINE <m.amine.osm@gmail.com>
2014-11-06 13:10:47 +0000

    redefinition du formulaire
    redefinition du formulaire etudiant (j'ai conserver les attribue nume
    ainsi les liens de action so don't worry Yunho :)  )

--- Pages/espace_etudiant.php
+++ Pages/espace_etudiant.php
@@ -1,15 +1,23 @@
 
 
-<fieldset>
-	<legend>Espace Etudiant</legend>
-	<pre>
-	<form action="Pages/informations_etudiant.php" method="post">
-		CNE : 
-			<input type="text" name="cne" />
-			
-			<input type="submit" value="Soumettre" />
-	</form>
-	</pre>
-</fieldset>
 
+<!-- S'authentifier-->
+	<div  class="row large-12 colums" width="500" data-reveal>
+		<h2>Espace Etudiant</h2>
+		<p class="lead"></p>
+		<div class="row">
+			<div class="large-6 columns">
+				<form data-abide role="form" method="POST" action='Pages/informations_etudiant.php' novalidate>
+				  <div class="name-field">
+				    <label>Votre CNE <small>obligatoire</small>
+				      <input type="password" id="cne" name="cne" required pattern="[0-9]{10}">
+				    </label>
+				    <small class="error">le CNE se compose de 10 chiffre.</small>
+				  </div>
+				  <button type="submit">Soumettre</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- S'authentifier-->
 
