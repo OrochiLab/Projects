12103e9e10aef3ae595b811abc817753fa3552a3
Mohammed Amine OUASMINE <m.amine.osm@gmail.com>
2014-11-08 17:27:47 +0000

    Modification

--- Pages/accueil.php
+++ Pages/accueil.php
@@ -2,16 +2,19 @@
 <ul class="example-orbit-content" data-orbit>
   <li data-orbit-slide="headline-1">
     <div>
+    	<h2></h2>
 		<img src="../Projects/img/demo3.jpg" alt="slide 2" />
     </div>
   </li>
   <li data-orbit-slide="headline-2">
     <div>
+    	<h2></h2>
       <img src="../Projects/img/demo1.jpg" alt="slide 2" />
     </div>
   </li>
   <li data-orbit-slide="headline-3">
     <div>
+    	<h2></h2>
 		<img src="../Projects/img/demo2.jpg" alt="slide 2" />
     </div>
   </li>
